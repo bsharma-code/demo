92d8b0741244f0bbe785b8b700954ddbbbd5096c
asus laptop <bhanusharma14581@gmail.com>
2025-07-09 11:57:14 +0530

    merged with remote

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,16 +1,8 @@
-from fastapi import APIRouter, HTTPException
-from app.schemas.user import LoginRequest
+from fastapi import APIRouter
 
 router = APIRouter()
 
-fake_user = {
-    "email": "bhanu@gamil.com",
-    "password": "123456"
-}
 
 @router.post("/login")
-def login_user(data: LoginRequest):
-    if data.email == fake_user["email"] and data.password == fake_user["password"]:
-        return {"message": "Login successful"}
-    else:
-        raise HTTPException(status_code=401, detail="Invalid email or password")
+def login_user():
+    pass
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,13 +1,1 @@
-from pydantic import BaseModel, EmailStr
-
-class LoginRequest(BaseModel):
-    email: EmailStr
-    password: str
-
-class UserResponse(BaseModel):
-    id: int
-    name: str
-    email: str
-
-    class Config:
-        orm_mode = True
+# schemas of the users
